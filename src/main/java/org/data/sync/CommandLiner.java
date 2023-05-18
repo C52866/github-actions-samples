ecd54115a559e42e3f8f1e7f421196cd74eae452
Kumar, Ram (Contractor) <R_Kumar@acs.org>
2023-05-18 18:24:59 +0530

    Sample Java App

---
+++ src/main/java/org/data/sync/CommandLiner.java
@@ -0,0 +1,16 @@
+package org.data.sync;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLiner implements CommandLineRunner {
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+	}
+
+}
